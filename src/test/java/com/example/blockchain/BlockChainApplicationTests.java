7b4d802593de23f97349e58fe564d30b53780cf8
신동현 <donghyeun02@gmail.com>
2024-04-12 15:02:59 +0900

    test : mining blocks

--- src/test/java/com/example/blockchain/BlockChainApplicationTests.java
+++ src/test/java/com/example/blockchain/BlockChainApplicationTests.java
@@ -18,4 +18,18 @@ class BlockChainTest {
         blockChain.getBlockChain().get(1).setData("Tampered Data");
         assertFalse(blockChain.isChainValid(), "데이터가 수정된 블록체인은 유효하지 않아야 합니다.");
     }
+
+    @DisplayName("블록 마이닝 테스트")
+    @Test
+    void testMining() {
+        BlockChain blockChain = new BlockChain();
+
+        blockChain.addBlock("Genesis Block", "0");
+
+        blockChain.addBlock("Block 1", blockChain.getLatestBlock().getHash());
+        blockChain.addBlock("Block 2", blockChain.getLatestBlock().getHash());
+
+        assertNotEquals(0, blockChain.getBlockChain().get(1).getNonce(), "블록 1의 난수는 0이 아니어야 합니다.");
+        assertNotEquals(0, blockChain.getBlockChain().get(2).getNonce(), "블록 2의 난수는 0이 아니어야 합니다.");
+    }
 }
